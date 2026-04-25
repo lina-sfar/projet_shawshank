7bfe9fbc5a8ad917f88ca87e8080c0420f1c3672
zeinebbenisma68-lab <zeinebbenisma68@gmail.com>
2026-04-26 01:56:32 +0200

    Update lot4.h

--- lot4/lot4.h
+++ lot4/lot4.h
@@ -2,6 +2,9 @@
 #define ENTETE_H_INCLUDED
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// ================= STRUCTS =================
 
 typedef struct
 {
@@ -16,13 +19,20 @@ typedef struct
 {
     SDL_Rect posob;
 } obstacle;
-void run_lot4(SDL_Renderer* ren);
+
+// ================= FUNCTIONS =================
+
+// main entry of lot4
+void run_lot4(SDL_Renderer* renderer);
+
 void initmap(minimap *m, SDL_Renderer *renderer);
 void MAJMinimap(minimap *m, SDL_Rect posPerso);
 void afficherminimap(minimap m, SDL_Renderer *renderer, int currentLevel);
 void freeMinimap(minimap *m);
+
 int collisionmap(SDL_Surface *masque, SDL_Rect posPerso, int direction);
 int collisionBB(SDL_Rect perso, SDL_Rect obstacle);
+
 void animation(minimap *m, int *currentLevel);
 
 #endif
