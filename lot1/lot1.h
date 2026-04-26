3f75296c6692f4fd1071329f4ddb94dd44a2953e
zeinebbenisma68-lab <zeinebbenisma68@gmail.com>
2026-04-26 19:48:46 +0200

    Update lot1.h

--- lot1/lot1.h
+++ lot1/lot1.h
@@ -7,6 +7,8 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+#define MAX_ANIM_FRAMES 3
+
 typedef enum {
     STATE_IDLE,
     STATE_WALKING,
@@ -16,22 +18,74 @@ typedef enum {
 } PlayerState;
 
 typedef struct {
+    // identity
+    int playerId;
+    char name[50];
+
+    // position / size
     float x, y;
     int w, h;
-    int health;
+
+    // physics
     float velX, velY;
     bool onGround;
     int direction;
+
+    // gameplay stats
+    int health;
+    int maxHealth;
+    int lives;
+    int score;
+
+    // state
     PlayerState state;
-    SDL_Texture* texture;
+
+    // combat
+    int attackCooldown;
+
+    // invincibility
+    bool isInvincible;
+    int invincibilityFrames;
+
+    // animation
+    int anim_frame;
+    int anim_timer;
+    int attack_frame;
+    int attack_timer;
+
+    // textures
+    SDL_Texture* walk_left[2];
+    SDL_Texture* walk_right[2];
+
+    SDL_Texture* attack_left[MAX_ANIM_FRAMES];
+    SDL_Texture* attack_right[MAX_ANIM_FRAMES];
+
+    SDL_Texture* jump_left;
+    SDL_Texture* jump_right;
+
+    SDL_Texture* idle_texture;
+
+    // controls
+    int keyLeft;
+    int keyRight;
+    int keyJump;
+    int keyAttack;
+    int keyRun;
+
 } Player;
 
-// ONLY ONE ENTRY POINT PER LOT
+// entry point
 void run_lot1(SDL_Renderer* renderer);
 
-// gameplay functions
-void init_player(Player* p);
+// core functions
+void init_player(Player* p, int id, const char* name, int startX, int startY);
 void update_player(Player* p);
 void render_player(SDL_Renderer* renderer, Player* p);
+
+// optional gameplay helpers (used in your .c)
+void load_player_animations(SDL_Renderer* renderer, Player* p);
+void update_player_animation(Player* p);
+void add_score(Player* p, int points);
+void take_damage(Player* p, int dmg);
 
 #endif
