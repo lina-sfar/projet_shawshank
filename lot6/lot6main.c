52312be1a74fb94c4e7079f4eda364b1794dd74b
zeinebbenisma68-lab <zeinebbenisma68@gmail.com>
2026-04-26 02:03:09 +0200

    Update lot6main.c

--- lot6/lot6main.c
+++ lot6/lot6main.c
@@ -1,84 +1,66 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "puzzle.h"
+#include <stdio.h>
 
-void lot6()
+void run_lot6(SDL_Renderer* renderer)
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
-    IMG_Init(IMG_INIT_PNG);
-
-    SDL_Window *win = SDL_CreateWindow(
-        "Puzzle Quiz",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WINDOW_W, WINDOW_H, 0);
-
-    SDL_Renderer *r = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-
     TTF_Font *font  = TTF_OpenFont("arial.ttf", 28);
     TTF_Font *small = TTF_OpenFont("arial.ttf", 18);
-    if (!font || !small) {
+
+    if (!font || !small)
+    {
         printf("Font error: %s\n", TTF_GetError());
-        return 1;
+        return;
     }
 
     Game g;
-    init_game(r, &g);
+    init_game(renderer, &g);
 
     int running = 1;
     SDL_Event e;
 
-    while (running) {
+    while (running)
+    {
         int mx, my;
         SDL_GetMouseState(&mx, &my);
 
-        while (SDL_PollEvent(&e)) {
+        while (SDL_PollEvent(&e))
+        {
             if (e.type == SDL_QUIT)
                 running = 0;
 
-            if (e.type == SDL_KEYDOWN) {
-                /* X to close when solved */
+            if (e.type == SDL_KEYDOWN)
+            {
                 if (e.key.keysym.sym == SDLK_x && g.solved)
                     running = 0;
 
-                /* T to try again when time is up */
                 if (e.key.keysym.sym == SDLK_t && g.time_up)
-                    new_round(r, &g);
+                    new_round(renderer, &g);
 
-                /* ESC always quits */
                 if (e.key.keysym.sym == SDLK_ESCAPE)
                     running = 0;
             }
 
-            if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT)
+            if (e.type == SDL_MOUSEBUTTONDOWN)
                 handle_mousedown(&g, e.button.x, e.button.y);
 
-            if (e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_LEFT)
+            if (e.type == SDL_MOUSEBUTTONUP)
                 handle_mouseup(&g, e.button.x, e.button.y);
 
             if (e.type == SDL_MOUSEMOTION)
                 handle_mousemove(&g, e.motion.x, e.motion.y);
         }
 
-        SDL_SetRenderDrawColor(r, 18, 20, 30, 255);
-        SDL_RenderClear(r);
+        SDL_SetRenderDrawColor(renderer, 18, 20, 30, 255);
+        SDL_RenderClear(renderer);
 
-        draw_game(r, font, small, &g, mx, my);
+        draw_game(renderer, font, small, &g, mx, my);
 
-        SDL_RenderPresent(r);
+        SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
 
     free_game(&g);
+
     TTF_CloseFont(font);
     TTF_CloseFont(small);
-    TTF_Quit();
-    IMG_Quit();
-    SDL_DestroyRenderer(r);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-    return 0;
 }
