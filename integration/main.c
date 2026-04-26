76939f2a4f2c8d47f5727cb4370ed5b107de71af
lina-sfar <lina.sfar@esprit.tn>
2026-04-26 20:09:49 +0100

    Update main.c

--- integration/main.c
+++ integration/main.c
@@ -11,9 +11,19 @@
 #include "../lot5/lot5.h"
 #include "../lot6/lot6.h"
 
+void afficherMenu() {
+    printf("\n=== SHAWSHANK REDEMPTION ===\n");
+    printf("1. Lancer le LOT 1 (Joueur)\n");
+    printf("2. Lancer le LOT 2 (Background)\n");
+    printf("3. Lancer le LOT 3 (Ennemis)\n");
+    printf("4. Lancer le LOT 4 (Mini map)\n");
+    printf("5. Lancer le LOT 5 (Quiz)\n");
+    printf("6. Lancer le LOT 6 (Puzzle)\n");
+    printf("0. Quitter\n");
+    printf("Votre choix : ");
+}
+
 int main() {
-    printf("=== Projet Shawshank ===\n");
-    
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     TTF_Init();
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
@@ -23,8 +33,21 @@ int main() {
                                        SDL_WINDOW_SHOWN);
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    // Lancer le lot1 pour l'instant (tu pourras faire un menu plus tard)
-    run_lot1(ren);
+    int choix;
+    do {
+        afficherMenu();
+        scanf("%d", &choix);
+        switch(choix) {
+            case 1: run_lot1(ren); break;
+            case 2: run_lot2(ren); break;
+            case 3: run_lot3(ren); break;
+            case 4: run_lot4(ren); break;
+            case 5: run_lot5(ren); break;
+            case 6: run_lot6(ren); break;
+            case 0: printf("Au revoir !\n"); break;
+            default: printf("Choix invalide.\n");
+        }
+    } while(choix != 0);
 
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
