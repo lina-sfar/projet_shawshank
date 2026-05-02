574f8b402db42b56d0374ba92611f90ed1299fd8
zeinebbenisma68-lab <zeinebbenisma68@gmail.com>
2026-05-02 23:06:34 +0200

    Update main.c

--- menu/principal/main.c
+++ menu/principal/main.c
@@ -1,4 +1,8 @@
-   #include "header.h"
+ #include "header.h"
+
+/* State (only what we need) */
+#define MENU_PRINCIPAL 0
+#define MENU_OPTIONS 1
 
 int main(int argc, char* argv[])
 {
@@ -8,25 +12,22 @@ int main(int argc, char* argv[])
 
     int running = 1;
 
-    // Init SDL
+    /* SDL INIT */
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         printf("SDL Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Init SDL_image
     if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
         printf("IMG Error: %s\n", IMG_GetError());
         return 1;
     }
 
-    // Init SDL_mixer
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
         printf("Mixer Error: %s\n", Mix_GetError());
         return 1;
     }
 
-    // Create window
     window = SDL_CreateWindow("Menu",
                               SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED,
@@ -39,40 +40,45 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // Create renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         printf("Renderer Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Init menu
     Menu menu;
     if (!initMenu(&menu, renderer)) {
         printf("Menu init failed\n");
         return 1;
     }
 
-    // Main loop
-    while (running) {
-
-        while (SDL_PollEvent(&e)) {
-
+    /* MAIN LOOP */
+    while (running)
+    {
+        while (SDL_PollEvent(&e))
+        {
             if (e.type == SDL_QUIT)
                 running = 0;
+        }
 
-            // Gestion du menu (Quit button déjà géré ici)
-            handleMenu(&menu, &e, &running);
+        /* Handle menu logic */
+        int action = handleMenu(&menu, &e);
+
+        if (action == -1)   /* quit */
+        {
+            running = 0;
+        }
+        else if (action == MENU_OPTIONS)   /* OPEN OPTIONS */
+        {
+            run_options(renderer);
         }
 
         SDL_RenderClear(renderer);
-
         renderMenu(&menu, renderer);
-
         SDL_RenderPresent(renderer);
     }
 
-    // Cleanup
+    /* CLEANUP */
     destroyMenu(&menu);
 
     SDL_DestroyRenderer(renderer);
